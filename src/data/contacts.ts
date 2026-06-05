661e2cebf9ba78b6c496fe3ad1025b2676df008e
ossiris11 <ostapchuknikita96@gmail.com>
2026-06-06 01:00:09 +0300

    Update Avito contact link

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -2,7 +2,7 @@ export const contacts = {
   vk: 'https://vk.me/tog_pc',
   telegram: 'https://t.me/tog_pc',
   instagram: 'https://www.instagram.com/tog.pc/',
-  avito: 'https://www.avito.ru/velikiy_novgorod?q=TOGOSHOL',
+  avito: 'https://www.avito.ru/brands/i112007990',
   email: 'mailto:hello@togoshol.ru',
 };
 
